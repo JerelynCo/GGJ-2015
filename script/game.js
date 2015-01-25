4bd1a18e3716122c489dae824f8bdac8966c6dc4
hadrianpaulo <hadrianpaulo@gmail.com>
2015-01-25 18:52:26 +0800

    Game structure

---
+++ script/game.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+
+     
+    }
+
+    function create () {
+
+     
+    }
+
+    function update() {
+
+    }
+
+    // Other functions declared here :)
+
+};
